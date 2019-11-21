57f913543401743d38d236ecac3aa63bd8a85d48
Mathieu Clément <mcfly1893@gmail.com>
2019-11-21 16:46:08 +0100

    Created a Hat prototype constructor and added a .toString() method to the prototype

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -7,11 +7,25 @@ let blueBaseballCap = {
   name: "Baseball cap",
   price: 11.99,
   color: "blue",
-  imgRef: "./assets/images/blue/hats/1.png",
+  imageHref: "./assets/images/blue/hats/1.png",
   toString: function() {
-    return `${this.name}, color ${this.color}, price: ${this.price}, image: ${this.imgRef}`;
+    return `${this.name}, color ${this.color}, price: ${this.price}, image: ${this.imageHref}`;
   }
 };
 
 // Check if the method works
 console.log(blueBaseballCap.toString());
+
+// Create a Hat prototype
+function Hat(name, price, color, imageHref) {
+  this.name = name;
+  this.price = price;
+  this.color = color;
+  this.imageHref = imageHref;
+}
+
+// Hat prototype methods
+// Method to return hat info as organised strings
+Hat.prototype.toString = function() {
+  return `${this.name}, color ${this.color}, price: ${this.price}, image: ${this.imageHref}`
+};
