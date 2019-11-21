646afa135a94d21b03212247075d48f5edaf1d0b
Mathieu Clément <mcfly1893@gmail.com>
2019-11-21 18:10:45 +0100

    Created displayHat() function and use it to render the hats from the hats array

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -45,3 +45,58 @@ let hats = [
   new Hat("Trilby", 10.99, "blue", "./assets/images/blue/hats/4.png"),
   new Hat("Trilby", 10.99, "yellow", "./assets/images/yellow/hats/4.png")
 ];
+
+// Function to display a hat object as a card on the product.html page
+const displayHat = function(hat) {
+  // Create first the HTML component piece by piece from the inner part
+  let cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = hat.name;
+
+  let itemColor = document.createElement("em");
+  itemColor.textContent = hat.color;
+
+  let cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.appendChild(document.createTextNode("Color: "));
+  cardText.appendChild(itemColor);
+
+  let cardButton = document.createElement("button");
+  cardButton.className = "btn btn-outline-primary";
+  cardButton.textContent = "Add to wishlist!";
+
+  let cardBody = document.createElement("div");
+  cardBody.className = "card-body text-center";
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardBody.appendChild(cardButton);
+
+  let cardImage = document.createElement("img");
+  cardImage.className = "card-img-top";
+  cardImage.src = hat.imageHref;
+  cardImage.alt = `Image of a ${hat.color} ${hat.name}`;
+
+  let priceTag = document.createElement("div");
+  priceTag.className = "currency btn btn-light disabled";
+  priceTag.textContent = hat.price;
+
+  let card = document.createElement("div");
+  card.className = "card my-3";
+  card.appendChild(priceTag);
+  card.appendChild(cardImage);
+  card.appendChild(cardBody);
+
+  let cardContainer = document.createElement("div");
+  cardContainer.className = "accessory col-sm-4";
+  cardContainer.appendChild(card);
+
+  // Get the HTML element that contains the products
+  // And append the new product to it
+  let productsList = document.getElementById("products");
+  productsList.appendChild(cardContainer);
+}
+
+// Render all the hats from hats array
+hats.forEach(hat => {
+  displayHat(hat);
+});
