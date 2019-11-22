fb8601cdbd588310d22f84e848c01c887f7948bd
Mathieu Clément <mcfly1893@gmail.com>
2019-11-22 02:00:42 +0100

    Implemented displayWishlist, renderWishlistItem and noWishlistFound functions in wishlist.js

--- assets/javascripts/wishlist.js
+++ assets/javascripts/wishlist.js
@@ -17,3 +17,80 @@ const getWishlist = function() {
     return wishlist;
   }
 };
+
+// Function that render wishlist's item (essentitally the same as the displayAccessory function)
+const renderWishlistItem = function(item) {
+  const accessory = item.accessory;
+
+  let cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = accessory.name;
+
+  let itemColor = document.createElement("em");
+  itemColor.textContent = accessory.color;
+
+  let cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.appendChild(document.createTextNode("Color: "));
+  cardText.appendChild(itemColor);
+
+  let cardButton = document.createElement("button");
+  cardButton.className = "btn btn-outline-danger";
+  cardButton.textContent = "Remove";
+  
+  let cardBody = document.createElement("div");
+  cardBody.className = "card-body text-center";
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardBody.appendChild(cardButton);
+
+  let cardImage = document.createElement("img");
+  cardImage.className = "card-img-top";
+  cardImage.src = accessory.imageHref;
+  cardImage.alt = `Image of a ${accessory.color} ${accessory.name}`;
+
+  let priceTag = document.createElement("div");
+  priceTag.className = "currency btn btn-light disabled";
+  priceTag.textContent = accessory.price;
+
+  let card = document.createElement("div");
+  card.className = "card my-3";
+  card.appendChild(priceTag);
+  card.appendChild(cardImage);
+  card.appendChild(cardBody);
+
+  let cardContainer = document.createElement("div");
+  cardContainer.className = "col-sm-4";
+  cardContainer.appendChild(card);
+
+  // Get the HTML element that contains the products
+  // And append the new product to it
+  let productsList = document.getElementById("products");
+  productsList.appendChild(cardContainer);
+};
+
+// Function that render warning message to inform user about his empty wishlist
+const noWishlistFound = function() {
+  // Create warning message
+  let warningMessage = document.createElement("p");
+  warningMessage.textContent = "No saved item in your wishlist. Go add some on the accessories page!";
+  warningMessage.style.color = "red";
+  // Render warning messeage
+  let productsList = document.getElementById("products");
+  productsList.appendChild(warningMessage);
+  productsList.style.justifyContent = "center";
+  productsList.style.padding = "3em";
+};
+
+// Function that display the wishlist
+const displayWishlist = function() {
+  const wishlist = getWishlist();
+  if (wishlist) {
+    wishlist.forEach(item => renderWishlistItem(item));
+  } else {
+    noWishlistFound();
+  }
+};
+
+// Call the displayWishlist function
+displayWishlist();
